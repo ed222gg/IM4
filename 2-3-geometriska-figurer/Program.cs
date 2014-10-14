33237134f6e793583f3caa7d0da2fbd2a8fba851
Caroline Forslund <info@carolineforslund.se>
2014-10-14 17:16:26 +0200

    new project, structure base

---
+++ 2-3-geometriska-figurer/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_3_geometriska_figurer
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+
+            do
+            {
+                ViewMenu();
+
+                switch (Console.ReadLine())
+                {
+                    case "0":
+                        return;
+
+                    case "1":
+                        //Ellipse
+                        Console.WriteLine("Ellipse");
+                        //CreateShape();
+                        break;
+
+                    case "2":
+                        //Rectangle
+                        Console.WriteLine("Rectangle");
+                        //CreateShape();
+                        break;
+
+                    default:
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.WriteLine("FEL! Ange ett nummer mellan 0 och 2");
+                        Console.ResetColor();
+                        break;
+                }
+                Console.ReadLine();
+            } while (true);
+        }
+
+        private static Shape CreateShape(ShapeType shapeType)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static double ReadDoubleGreaterThanZero(string promt)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void ViewMenu()
+        {
+
+
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("===========================");
+            Console.WriteLine("=                         =");
+            Console.WriteLine("=   Geometriska figurer   =");
+            Console.WriteLine("=                         =");
+            Console.WriteLine("===========================");
+            Console.ResetColor();
+            Console.WriteLine();
+            Console.WriteLine("0. Avsluta");
+            Console.WriteLine("1. Ellips");
+            Console.WriteLine("2. Reklangel");
+            Console.WriteLine();
+            Console.WriteLine("===========================");
+            Console.WriteLine();
+            Console.Write("Ange menyval 0-2: ");
+        }
+
+        private static void ViewShapeDetail(Shape shape)
+        {
+            throw new NotImplementedException();
+        }
+
+        //private static void ViewErrorMessage(string prompt)
+        //{
+        //    Console.BackgroundColor = ConsoleColor.Red;
+        //    Console.ForegroundColor = ConsoleColor.White;
+        //    Console.WriteLine(prompt);
+        //    Console.ResetColor();
+        //}
+    }
+}
---
+++ 2-3-geometriska-figurer/Shape.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_3_geometriska_figurer
+{
+    enum ShapeType { Ellipse, Rectangle }
+
+    abstract class Shape
+    {
+        //Deklarera fält
+        private double _length;
+        private double _width;
+
+        ////Egenskaperna Area, Length, Perimeter och Width
+        //public abstract double Area
+        //{
+        //    get { return 0; } //?
+        //}
+
+        public double Length
+        {
+            get { return _length; }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException();
+                }
+                _length = value;
+            }
+        }
+
+        //public abstract double Perimeter
+        //{ 
+        //    get { return 0; } //?
+        //}
+
+        public double Width
+        {
+            get { return _width; }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException();
+                }
+                _width = value;
+            }
+        }
+
+        ////Metoderna Shape(konstruktorn) och ToString
+        //protected Shape(double length, double width)
+        //{
+
+        //}
+
+        //public override string ToString()
+        //{
+        //    //throw new NotImplementedException();
+
+        //    string result;
+
+        //    result = String.Format("Längd   : {0}\nBredd   : {1}\nOmkrets : {2}\nArea    : {3}",
+        //        Length, Width, Perimeter, Area);
+
+        //    return result;
+
+        //    //Console.WriteLine("Längd   : {0}", Length);
+        //    //Console.WriteLine("Bredd   : {0}", Width);
+        //    //Console.WriteLine("Omkrets : {0}", Perimeter);
+        //    //Console.WriteLine("Area    : {0}", Area);
+        //}
+    }
+}
